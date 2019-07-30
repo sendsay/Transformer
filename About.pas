ff5009d64fa71b885e3c20f92c729287017246ce
sendsay <sendsay@yahoo.com>
2019-07-30 09:39:49 +0300

    Start NOW

---
+++ About.pas
@@ -0,0 +1,39 @@
+unit About;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Imaging.pngimage,
+  Vcl.ExtCtrls;
+
+type
+  TAboutForm = class(TForm)
+    Panel1: TPanel;
+    ProgramIcon: TImage;
+    ProductName: TLabel;
+    Version: TLabel;
+    Copyright: TLabel;
+    Comments: TLabel;
+    OKButton: TButton;
+    Label1: TLabel;
+    procedure OKButtonClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  AboutForm: TAboutForm;
+
+implementation
+
+{$R *.dfm}
+
+procedure TAboutForm.OKButtonClick(Sender: TObject);
+begin
+  AboutForm.Close;
+end;
+
+end.
